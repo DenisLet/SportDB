b454cb6a5b819b992306e1d34d467db26a4bcd2b
jonny <den25ru@gmail.com>
2024-01-31 15:11:41 +0100

    feat. failed handler

--- failed_run.py
+++ failed_run.py
@@ -4,17 +4,16 @@ from sport_handlers.main_handler import MatchHandler
 
 
 urls = [
-"https://www.basketball24.com/south-korea/kbl",
-"https://www.basketball24.com/sweden/basketligan",
-"https://www.basketball24.com/switzerland/sb-league"
+
+"https://www.basketball24.com/chile/lnb",
+
 ]
 
 
 for url in urls:
-    subprocess.run(['python', 'link_collector.py', url], check=True)
     file_name = '-'.join(url.split('/')[-2:])
     print(file_name)
-    with open(f"details\\txt_links\\{file_name}.txt", 'r') as file:
+    with open(f"details\\txt_links\\failed\\{file_name}.txt", 'r') as file:
         links = [line.strip() for line in file.readlines()]
     basketball_handler = Basketball(links, url)
     basketball_handler.open_browser_and_process_links()
